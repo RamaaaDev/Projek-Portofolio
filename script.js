be9a6013f53c945e38e155ad67e1b77c228d4ddd
RamaaaDev <122418599+RamaaaDev@users.noreply.github.com>
2023-06-06 03:01:59 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
-const txtElement =['gamer', 'mahasiswa', 'developer'];
-let count = 0;
-let txtIndex = 0;
-let currentTxt = '';
-let word = '';
+// ========== toggle icon navbar =============
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
-(function ngetik(){
-    if(count == txtElement.length){
-        count = 0;
-    }
-    currentTxt = txtElement[count];
-    word = currentTxt.slice(0, ++txtIndex);
-    document.querySelector('.type').textContent = word;
-    if(word.length == currentTxt.length){
-        count++;
-        txtIndex = 0;
-    }
-    setTimeout(ngetik, 470);
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
 
+// ========== Scroll section active link =============
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
-})();
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttrribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+// ========== Sticky navbar =============
+    let header = document.querySelector('header');
+    
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+// remove toggle icon and navbar when click navbar link 
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
+};
+
